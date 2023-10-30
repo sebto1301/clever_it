ba12647db606f1a2760ea88fe5ca07f7df977c28
sebto1301 <s.torres.d@gmail.com>
2023-10-30 01:18:47 -0300

    Changed sqlite database as default

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 JWT_KEY = "ThisIsTheCoolestTaskTagUserAPI"
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@db/tasktag'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@db/tasktag'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 
 app.config['JWT_SECRET_KEY'] = JWT_KEY
 
